3c08e40230cdaf86736735d3781ecd37035f0120
Kyle Pfromer <kpfromer2@gmail.com>
2019-02-10 05:24:28 -0700

    Fix user model

--- src/modules/models/user.ts
+++ src/modules/models/user.ts
@@ -1,17 +1,24 @@
 import { Document, Schema, Model, model} from "mongoose";
+import { IBid } from "./bid";
 
 export interface IUser {
   name: string;
   email: string;
   phone: string;
   address: string;
+  password: string;
 }
 
-export interface IUserModel extends Document, IUser {}
+export interface IUserModel extends Document, IUser {
+  bids: IBid[]
+}
 
 const schema: Schema = new Schema({
-  amount: { required: true, type: Number },
-  donations: [{ type: Schema.Types.ObjectId, ref: 'donation' }],
+  name: { required: true, type: String },
+  email: { required: true, type: String },
+  phone: { required: true, type: String },
+  address: { required: true, type: String },
+  password: { required: true, type: String },
   bids: [{ type: Schema.Types.ObjectId, ref: 'bid' }]
 });
 
