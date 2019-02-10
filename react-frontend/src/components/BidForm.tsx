449ab8430073587b3d95b162da7fcf9a0ab35260
Kyle Pfromer <kpfromer@users.noreply.github.com>
2019-02-10 16:46:44 -0700

    Add bid form

---
+++ react-frontend/src/components/BidForm.tsx
@@ -0,0 +1,38 @@
+import React, { Component, FormEvent, ChangeEvent } from 'react';
+import '../css/Auction.css';
+
+export interface BidProps {
+  value: number;
+  onChange: (bidPrice: number) => any;
+  onSubmit: () => any;
+}
+
+export default class BidForm extends Component<BidProps> {
+  state = {
+    error: false
+  }
+  handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const price: number = parseFloat(event.target.value);
+    this.props.onChange(price);
+    if (isNaN(price)) {
+      this.setState({ error: 'Bid is not a number.' });
+    } else {
+      this.setState({ error: false });
+    }
+  }
+  handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    if (!this.state.error) {
+      this.props.onSubmit();
+    }
+  }
+  render() {
+    const { value } = this.props;
+    return (
+      <form className="auctionForm" onSubmit={this.handleSubmit}>
+        <input type="number" value={value} onChange={this.handleChange} placeholder="Make a bid"/>
+        <input type="submit" value="Bid" />
+      </form>
+    );
+  }
+}
