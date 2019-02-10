9d969be68ee2c4ee4d297baff1aa0ccc7ebcd967
Kyle Pfromer <kpfromer2@gmail.com>
2019-02-10 05:25:11 -0700

    Fix bid model

--- src/modules/models/bid.ts
+++ src/modules/models/bid.ts
@@ -1,10 +1,15 @@
 import { Document, Schema, Model, model} from "mongoose";
+import { IUserModel } from "./user";
+import { IItemModel } from "./item";
 
 export interface IBid {
   amount: number;
 }
 
-export interface IBidModel extends Document, IBid {}
+export interface IBidModel extends Document, IBid {
+  user: IUserModel;
+  item: IItemModel;
+}
 
 const schema: Schema = new Schema({
   amount: { required: true, type: Number },
