c40b1299b0e9ddff88cfd1b7fc599689d874c6fe
Gernene <wallie988@gmail.com>
2019-02-10 00:03:58 -0700

    Create sign up component

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './css/App.css';
 import Auction from "./components/Auction";
+import SignUp from "./components/SignUp";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Silent Auction</h1>
+        <SignUp/>
         <Auction/>
         <Auction/>
         <Auction/>
