778f32e9a74479c0b94d2c6892a077f2e70c94d2
Kyle Pfromer <kpfromer2@gmail.com>
2019-02-10 05:21:16 -0700

    Update app to use passport

--- src/app.ts
+++ src/app.ts
@@ -4,32 +4,41 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import path from 'path';
+import passport from 'passport';
+import jwtStrategy from './modules/auth/passport';
 import error from './modules/error/error';
+import auth from './modules/routes/auth';
+import bids from './modules/routes/bid';
 
 // Mongoose Setup
-mongoose.connect('mongourl://localhost:27017/silent-auction', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/silent-auction', { useNewUrlParser: true });
+
+// Passport setup (authentication)
+passport.use(jwtStrategy());
 
 // Express Config
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.use(passport.initialize());
 
 // API Routes
 const apiRoute = express.Router();
 
-apiRoute.use('/auction', auctions);
+apiRoute.use('/auth', auth);
+// apiRoute.use('/auction', auctions);
 apiRoute.use('/bid', bids);
-apiRoute.use('/donation', donations);
-apiRoute.use('/item', items);
-apiRoute.use('/user', users);
+// apiRoute.use('/donation', donations);
+// apiRoute.use('/item', items);
+// apiRoute.use('/user', users);
 
 app.use('/api', apiRoute);
 
 // Route every route to React (expect for api routes)
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
-  res.sendfile(path.join(__dirname, 'build/index.html'));
+  res.sendFile(path.join(__dirname, 'build/index.html'));
 });
 
 // API Error handling
