6a1156900763568e0b450984bcd304195430d08c
Gernene <wallie988@gmail.com>
2019-02-10 19:55:03 -0700

    Restyle auctions to accomodate logos

--- react-frontend/src/components/Auction.tsx
+++ react-frontend/src/components/Auction.tsx
@@ -21,31 +21,34 @@ class Auction extends Component<AuctionProps> {
     const { startingPrice, highestPrice, enteredPrice, canBid, onChange, onSubmit } = this.props;
     return (
       <div className="auction">
-        <h2 className="auctionName">Bundle Name</h2>
-        <img src="http://style.anu.edu.au/_anu/4/images/placeholders/person.png" className="auctionImg"/>
-        <p className="auctionDesc">
-          Treat yourself to some well-deserved relaxation and happiness after all your hard work! Indulge in a gift basket of chocolates and bath salts from Premier Commercial Interiors, then get a refreshing acupuncture session from Inner Path Acupuncture. Try an hour of deeply calming flotation therapy at Isolate to recover from any stress in your life. 
-        </p>
-        <div className="auctionStart">
-            Starting Price:
-            <div className="auctionStartVal">
-                {this.formatMoney(startingPrice)}
-            </div>
+        <div className="auctionL">
+          <h2 className="auctionName">Bundle Name</h2>
+          <p className="auctionDesc">
+            Treat yourself to some well-deserved relaxation and happiness after all your hard work! Indulge in a gift basket of chocolates and bath salts from Premier Commercial Interiors, then get a refreshing acupuncture session from Inner Path Acupuncture. Try an hour of deeply calming flotation therapy at Isolate to recover from any stress in your life. 
+          </p>
+          <div className="auctionStart">
+              Starting Price:
+              <div className="auctionStartVal">
+                  {this.formatMoney(startingPrice)}
+              </div>
+          </div>
+          <div className="auctionHighest">
+              Highest Bid:
+              <div className="auctionHighestVal">
+                  {this.formatMoney(highestPrice)}
+              </div>
+          </div>
+          {
+            canBid &&
+            <BidForm 
+              onChange={onChange}
+              onSubmit={onSubmit}
+              value={enteredPrice}
+            />
+          }
         </div>
-        <div className="auctionHighest">
-            Highest Bid:
-            <div className="auctionHighestVal">
-                {this.formatMoney(highestPrice)}
-            </div>
+        <div className="auctionR">
         </div>
-        {
-          canBid &&
-          <BidForm 
-            onChange={onChange}
-            onSubmit={onSubmit}
-            value={enteredPrice}
-          />
-        }
       </div>
     );
   }
