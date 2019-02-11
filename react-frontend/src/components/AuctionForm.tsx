a10d8f32eca67c23dce129ee2ef6ac191aa5ba96
Kyle Pfromer <kpfromer@users.noreply.github.com>
2019-02-10 21:01:46 -0700

    Fix AuctionForm types

--- react-frontend/src/components/AuctionForm.tsx
+++ react-frontend/src/components/AuctionForm.tsx
@@ -2,14 +2,12 @@ import React, { Component, ChangeEvent, FormEvent } from 'react';
 import '../css/ACP.css';
 import Datetime from 'react-datetime';
 import { Moment } from 'moment';
+import { AuctionType, Omit } from '../App';
 
 export type AuctionItems = 'name' | 'image' | 'startingPrice' | 'description' | 'startTime' | 'endTime';
 
-export interface AuctionFormProps {
-  name: string;
-  image: string;
-  startingPrice: number;
-  description: string;
+
+export type AuctionFormProps = Omit<AuctionType, 'startTime' | 'endTime'> & {
   startTime: string | Moment;
   endTime: string | Moment;
   onChange: (property: AuctionItems, value: any) => any;
@@ -17,7 +15,6 @@ export interface AuctionFormProps {
 }
 
 class AuctionForm extends Component<AuctionFormProps> {
-
   handleChange = (property: AuctionItems) => (event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => {
     let value: any = event.target.value;
     if (property === 'startingPrice') {
@@ -31,12 +28,6 @@ class AuctionForm extends Component<AuctionFormProps> {
   handleSubmit = (event: FormEvent) => {
     event.preventDefault();
     this.props.onSubmit();
-  }
-  dateToString = (rawDate: Date): string => {
-    return rawDate.toISOString().slice(0, -1);
-    // const date = moment(rawDate);
-    // console.log(date)
-    // return date.toStr;
   }
   render() {
     const { name, image, startingPrice, description, startTime, endTime } = this.props;
@@ -52,10 +43,8 @@ class AuctionForm extends Component<AuctionFormProps> {
         <textarea placeholder="Bundle Description" value={description} onChange={this.handleChange('description')} required/>
         <label>Start Time:</label>
         <Datetime value={startTime} onChange={this.handleDateChange('startTime')} />
-        {/* <input type="datetime-local" placeholder="Start Time" value={this.dateToString(startTime)} onChange={this.handleChange('startTime')} required/> See https://stackoverflow.com/questions/12346381/set-date-in-input-type-date */}
         <label>End Time:</label>
         <Datetime value={endTime} onChange={this.handleDateChange('endTime')} />
-        {/* <input type="datetime-local" placeholder="End Time" value={this.dateToString(endTime)} onChange={this.handleChange('endTime')} required/> */}
         <input type="submit" value="Submit" required/>
       </form>
     );
